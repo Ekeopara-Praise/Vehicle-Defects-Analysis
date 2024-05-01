330438774c16653331e8ccc5acbc30a22c7e9fbc
Ekeopara-Praise <ekeoparapraise@gmail.com>
2024-05-01 01:58:26 +0100

    modifid app

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print('testing tools!')
+# import libraries
+import pandas as pd 
+import numpy as np
+import joblib as jb 
+import streamlit as st
+
+# define code 1: Get data
+
+
+# define code 2: Preprocess data
+
+
+# define code 3: Make prediction
+
+
+# deine code 4: Handle UI operations
